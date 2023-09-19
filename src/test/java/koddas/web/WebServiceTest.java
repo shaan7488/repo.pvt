e5f1fa942a90e3cf1ff28eef74cbbad8750055ba
shaan7488 <119840304+shaan7488@users.noreply.github.com>
2023-09-19 13:53:14 +0530

    Update WebServiceTest.java

--- src/test/java/koddas/web/WebServiceTest.java
+++ src/test/java/koddas/web/WebServiceTest.java
@@ -1,1 +1,26 @@
+package koddas.web.war;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import javax.ws.rs.core.Response;
+
+public class WebServiceTest {
+    private WebService webService;
+
+    @Before
+    public void setUp() {
+        webService = new WebService();
+    }
+
+    @Test
+    public void testHello() {
+        Response response = webService.hello();
+        assertEquals(200, response.getStatus());
+        assertEquals("Hello, World!", response.getEntity());
+    }
+
+    // You can add more test methods for other WebService methods here if needed
+}
 
